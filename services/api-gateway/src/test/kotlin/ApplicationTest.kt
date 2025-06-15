ca017c2117f4cdbe971cbf1fd69a365410c47d9f
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2025-06-14 22:06:03 -0300

    Add initial project of api-gateway

---
+++ services/api-gateway/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package br.com.sailtech.tito.api
+
+import br.com.sailtech.tito.api.br.com.sailtech.tito.api.gateway.module
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+
+}
